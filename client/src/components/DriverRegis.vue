605f02ff68e0c37930270d7a2d8c57455397e276
Benybenben <Thanawanbn@gmail.com>
2020-01-08 16:07:53 +0700

    close #31 -Do DriverRegis UI

---
+++ client/src/components/DriverRegis.vue
@@ -0,0 +1,306 @@
+<template>
+    <v-container>
+      <v-card style="width:90%; margin:auto; background-color:#FFFFFF">
+        <v-form
+                ref="form"
+                v-model="valid"
+        >
+            <v-layout
+                    text-center
+                    wrap
+            >
+                <v-flex xs12>
+                    <v-img
+                            :src="require('../assets/driver.png')"
+                            class="my-3"
+                            contain
+                            height="100"
+                    ></v-img>
+                </v-flex>
+
+                <v-container>
+                    <div>
+                        <p class="font-weight-black display-1">DRIVER REGISTER</p>
+                    </div>
+                </v-container>
+                
+                <v-container>
+                    <v-row justify="center">
+                        <v-col cols="12" sm="4">
+                            <p>Name</p>
+                            <v-text-field
+                                        v-model="driverRegis.driverName"
+                                        :rules="[(v) => !!v || 'Item is required']"
+                                        label="-- ชื่อ-นามสกุล --"
+                                        required
+                            ></v-text-field>
+                          </v-col>
+                          
+                          <v-col cols="12" sm="4">
+                            <v-menu
+                            v-model="menu"
+                            :close-on-content-click="false"
+                            :nudge-right="40"
+                            transition="scale-transition"
+                            offset-y
+                            min-width="120px"
+                            >
+                            <template v-slot:activator="{ on }">
+                              <p>Birthday</p>
+                              <v-text-field style= "width:80%"
+                                  prepend-icon= "mdi mdi-calendar"
+                                  v-model="date"
+                                  label="วัน-เดือน-ปีเกิด"
+                                  clearable
+                                  readonly
+                                  v-on="on"
+                                ></v-text-field>
+                              </template>
+                              <v-date-picker v-model="date" @input="menu = false"></v-date-picker>
+                            </v-menu>
+                         </v-col>
+                    </v-row>
+                </v-container>
+
+
+                 <v-container>
+                    <v-row justify="center">
+                        <v-col cols="12" sm="4">
+                            <p>ID Card</p>
+                            <v-text-field
+                                        v-model="driverRegis.driverCard"
+                                        :rules="[(v) => !!v || 'Item is required']"
+                                        label="-- เลขที่บัตรประชาชน --"
+                                        required
+                            ></v-text-field>
+                        </v-col>
+
+                        <v-col cols="12" sm="4">
+                            <p>Gender</p>
+                            <v-select
+                                    v-model="driverRegis.driverGender"
+                                    :items="genders"
+                                    item-text="sex"
+                                    item-value="genderid"
+                                    :rules="[v => !!v || 'Item is required']"
+                                    label="-- เพศ --"
+                                    required
+                            ></v-select>
+                        </v-col>
+                    </v-row>
+                </v-container>
+
+
+                <v-container>
+                    <v-row justify="center">
+                        <v-col cols="12" sm="4">
+                            <p>Address</p>
+                            <v-text-field
+                                        v-model="driverRegis.driverAddress"
+                                        :rules="[(v) => !!v || 'Item is required']"
+                                        label="-- ที่อยู่ --"
+                                        required
+                            ></v-text-field>
+                        
+                        </v-col>
+                        <v-col cols="12" sm="4">
+                            <p>Telephone</p>
+                            <v-text-field
+                                        v-model="driverRegis.driverTelephone"
+                                        :rules="[(v) => !!v || 'Item is required']"
+                                        label="-- เบอร์โทรศัพท์มือถือ --"
+                                        required
+                            ></v-text-field>
+                        </v-col>
+
+                    </v-row>
+                </v-container>
+
+                <v-container>
+                    <v-row justify="center">
+                        <v-col cols="12" sm="4">
+                          <p>Provinces</p>
+                            <v-select
+                                    v-model="driverRegis.provinceId"
+                                    :items="provinces"
+                                    item-text="provincename"
+                                    item-value="provinceid"
+                                    :rules="[v => !!v || 'Item is required']"
+                                    label="-- จังหวัด --"
+                                    required
+                            ></v-select>
+                        </v-col>
+
+                        <v-col cols="12" sm="4">
+                            <p>Educational</p>
+                            <v-select
+                                    v-model="driverRegis.educationalId"
+                                    :items="educationals"
+                                    item-text="educationallevel"
+                                    item-value="educationalid"
+                                    :rules="[v => !!v || 'Item is required']"
+                                    label="-- วุฒิการศึกษา --"
+                                    required
+                            ></v-select>
+                        </v-col>
+                    </v-row>
+                </v-container>
+
+                    <v-container>
+                    <v-row justify="center">
+                        <v-col cols="12" sm="5">
+                            <p>Position Driver</p>
+                            <v-select
+                                    v-model="driverRegis.positionId"
+                                    :items="positiondrivers"
+                                    item-text="positionlevel"
+                                    item-value="positionDriverid"
+                                    :rules="[v => !!v || 'Item is required']"
+                                    label="-- ตำแหน่งพนักงานขับ --"
+                                    required
+                            ></v-select>
+                        </v-col>
+                    </v-row>
+                </v-container>            
+                <v-row justify="center">
+                    <v-col cols="12">
+                        <v-btn :disabled="!valid" color="success" @click="saveDriverRegists">Submit</v-btn>
+                        <v-btn style="margin-left: 15px;" color="yellow darken-1" @click="viewinfo">Info.</v-btn>
+                        <v-btn style="margin-left: 15px;" @click="clear">Reset</v-btn>
+                    </v-col>
+                </v-row>
+            </v-layout>
+        </v-form>
+        </v-card>
+    </v-container>
+
+</template>
+
+<script>
+import http from "../http-common";
+/* eslint-disable */
+export default {
+  name: "driverRegis",
+  data() {
+    return {
+        date : new Date().toISOString().substr(0, 10),
+        menu : false,
+        driverRegis : {
+        driverName : null,
+        driverCard : null,
+        driverGender : null,
+        driverAddress : null,
+        driverTelephone : null,
+        provinceId : null,
+        positionId : null,
+        educationalId : null
+      },
+        educationals : null,
+        positiondrivers : null,
+        genders : null,
+        provinces : null,
+        valid: false
+    };
+  },
+
+  methods: {
+     getGenders() {
+      http
+        .get("/gender")
+        .then(response => {
+          this.genders = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    
+    },
+    getPosiTions() {
+      http
+        .get("/positiondriver")
+        .then(response => {
+          this.positiondrivers = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+     },
+     getProvinces() {
+      http
+        .get("/province")
+        .then(response => {
+          this.provinces = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    getEducatioNals() {
+      http
+        .get("/educational")
+        .then(response => {
+          this.educationals = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+   },
+      saveDriverRegists() {
+      http
+        .post(
+          "/driverRegis/" +
+            this.date +
+            "/" +
+            this.driverRegis.driverName +
+            "/" +
+            this.driverRegis.driverCard +
+            "/" +
+            this.driverRegis.driverAddress +
+            "/" +
+            this.driverRegis.driverTelephone +
+            "/" +
+            this.driverRegis.driverGender +
+            "/" +
+            this.driverRegis.positionId +
+             "/" +
+            this.driverRegis.provinceId +
+             "/" +
+            this.driverRegis.educationalId,
+        this.driverRegis
+        )
+        .then(response => {
+          console.log(response);
+          this.$router.push("/viewdriver");
+          alert('บันทึกข้อมูลสำเร็จ');
+          this.$refs.form.reset();
+        })
+        .catch(e => {
+          console.log(e);
+          alert('บันทึกข้อมูลไม่สำเร็จ');
+        });
+      this.submitted = true;
+
+          if (this.$refs.form.validate()) {
+              this.snackbar = true
+          }
+    },
+      viewinfo() {
+          this.$router.push("/viewdriver");
+      },
+    clear() {
+      this.$refs.form.reset();
+    }
+  },
+  mounted() {
+      this.getGenders();
+      this.getPosiTions();
+      this.getProvinces();
+      this.getEducatioNals();
+      
+  }
+};
+</script>
--- client/src/components/Emergency.vue
+++ client/src/components/Emergency.vue
@@ -11,7 +11,7 @@
             >
                 <v-flex xs12>
                     <v-img
-                            :src="require('../assets/Emergency.png')"
+                            :src="require('../assets/ambulance.png')"
                             class="my-3"
                             contain
                             height="100"
@@ -20,14 +20,14 @@
 
                 <v-container>
                     <div>
-                        <p class="font-weight-black display-1">รับเรื่องฉุกเฉิน</p>
+                        <p class="font-weight-black display-1">ADD EMERGENCY</p>
                     </div>
                 </v-container>
 
                 <v-container>
                     <v-row justify="center">
                         <v-col cols="12" sm="4">
-                            <p>ชื่อผู้ป่วย</p>
+                            <p>PatientName</p>
                             <v-text-field
                                         v-model="emergency.emergencyPatientName"
                                         :rules="[(v) => !!v || 'Item is required']"
@@ -36,7 +36,7 @@
                             ></v-text-field>
                         </v-col>
                         <v-col cols="12" sm="4">
-                            <p>เบอร์โทรศัพท์ผู้ป่วย</p>
+                            <p>PatientTel</p>
                             <v-text-field
                                         v-model="emergency.emergencyPatientTel"
                                         :rules="[(v) => !!v || 'Item is required']"
@@ -50,7 +50,7 @@
                 <v-container>
                         <v-row justify="center">
                             <v-col cols="12" sm="8">
-                                 <p>ตำแหน่งฝที่อยู่ผู้ป่วย</p>
+                                 <p>Location</p>
                                     <v-text-field
                                         v-model="emergency.emergencyPatientAddress"
                                         :rules="[(v) => !!v || 'Item is required']"
@@ -64,23 +64,23 @@
                 <v-container>
                   <v-row justify="center">
                         <v-col cols="8" sm="4">
-                            <p>ลักษณะอาการ</p>
+                            <p>Symptom Type</p>
                             <v-select
                                     v-model="emergency.symptomId"
                                     :items="symptoms"
                                     item-text="type"
                                     item-value="symptomid"
                                     :rules="[v => !!v || 'Item is required']"
-                                    label="-- ประเภทอาการผู้ป่วย -- "
+                                    label="-- ประเภทอาการผู้ป่วย --"
                                     required
                             ></v-select>
                         </v-col>
                         <v-col cols="12" sm="4">
-                            <p>หมายเหตุเพิ่มเติม</p>
+                            <p>Note</p>
                             <v-text-field
                                         v-model="emergency.emergencyNote"
                                         :rules="[(v) => !!v || 'Item is required']"
-                                        label="-- อาการอย่างอื่นโปรดระบุ -- "
+                                        label="-- หมายเหหตุ --"
                                         required
                              ></v-text-field>
                         </v-col>
@@ -89,28 +89,28 @@
 
                 <v-container>
                     <v-row justify="center">                       
-                        <v-col cols="8" sm="4">
-                            <p>รถพยาบาลฉุกเฉิน</p>
+                        <v-col cols="12" sm="4">
+                            <p>Ambulance</p>
                             <v-select
                                     v-model="emergency.ambulanceId"
                                     :items="ambulances"
                                     item-text="licenseplate"
                                     item-value="ambulanceid"
                                     :rules="[v => !!v || 'Item is required']"
-                                    label="-- ทะเบียนรถพยาบาล --"
+                                    label="-- รถฉุกเฉิน --"
                                     required
                             ></v-select>
                         </v-col>
 
-                        <v-col cols="8" sm="4">
-                            <p>คนขับรถพยาบาล</p>
+                        <v-col cols="12" sm="4">
+                            <p>Driver</p>
                             <v-select
                                     v-model="emergency.driverregisId"
                                     :items="driverRegiss"
                                     item-text="drivername"
                                     item-value="driverregisid"
                                     :rules="[v => !!v || 'Item is required']"
-                                    label="-- ชื่อคนขับรถ --"
+                                    label="-- คนขับรถ --"
                                     required
                             ></v-select>
                         </v-col>
@@ -119,21 +119,21 @@
 
                  <v-container>
                     <v-row justify="center">
-                        <v-col cols="8" sm="4">
-                            <p>นางพยาบาลรับเรื่อง</p>
+                        <v-col cols="10" sm="5">
+                            <p>Nurse</p>
                             <v-select
                                     v-model="emergency.nurseId"
                                     :items="nurses"
                                     item-text="nursename"
                                     item-value="nurseid"
                                     :rules="[v => !!v || 'Item is required']"
-                                    label="-- ชื่อพยาบาลรับเรื่องฉุกเฉิน --"
+                                    label="-- พยาบาลรับเรื่องฉุกเฉิน --"
                                     required
                             ></v-select>
                         </v-col>
                     </v-row>
                 </v-container>
-                
+
                 <v-row justify="center">
                     <v-col cols="12">
                         <v-btn :disabled="!valid" color="success" @click="saveEmergencys">Submit</v-btn>
@@ -141,6 +141,8 @@
                         <v-btn style="margin-left: 15px;" @click="clear">Reset</v-btn>
                     </v-col>
                 </v-row>
+
+
             </v-layout>
         </v-form>
         </v-card>
