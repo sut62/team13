23e18d1c39513697ede2f38f6960c05b70c84e07
ChayanonPiakhuntod <chayanonb5814664@gmail.com>
2020-01-21 04:23:59 +0700

    clsoe #82 - add AmbulanceDeleted Controller and edit Entity

--- backend/src/main/java/com/sut/se/g13/Repository/AmbulanceDeletedRepository.java
+++ backend/src/main/java/com/sut/se/g13/Repository/AmbulanceDeletedRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface AmbulanceDeletedRepository extends JpaRepository<AmbulanceDeleted, Long> {
-    Ambulance findByDeletedId(long deletedid);
+
 }
