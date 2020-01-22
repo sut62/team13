395906ca9df888a5ea8befa152e4da96984ad467
ChayanonPiakhuntod <chayanonb5814664@gmail.com>
2020-01-23 06:03:10 +0700

    close #84 - Add AmbulanceDeleted Test case

--- backend/src/main/java/com/sut/se/g13/Entity/AmbulanceDeleted.java
+++ backend/src/main/java/com/sut/se/g13/Entity/AmbulanceDeleted.java
@@ -4,6 +4,10 @@ import lombok.*;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import java.util.Date;
@@ -23,7 +27,14 @@ public class AmbulanceDeleted {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "deleted_seq")
     @Column(name = "DELETED_ID", unique = true, nullable = true)
     private @NonNull Long deletedid;
-    private @NonNull Date deletedtime;
-    private @NonNull String employee;
-    private @NonNull Long ambulance;
+    
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    private  Date deletedtime;
+
+    @Size(min = 2, max = 20)
+    private @NotNull String employee;
+
+    @Column(name = "AMBULANCE_ID", unique = true, nullable = true)
+    private @NotNull Long ambulance;
 }
