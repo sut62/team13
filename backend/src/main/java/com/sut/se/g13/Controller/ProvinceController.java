89b8c352f0fb749679ae4923340b59cc206a4f93
ChayanonPiakhuntod <chayanonb5814664@gmail.com>
2020-01-26 11:03:19 +0700

    close #84 - edit Ambulance Controller

--- backend/src/main/java/com/sut/se/g13/Controller/ProvinceController.java
+++ backend/src/main/java/com/sut/se/g13/Controller/ProvinceController.java
@@ -1,26 +1,22 @@
 package com.sut.se.g13.Controller;
-
+import com.sut.se.g13.Entity.Province;
+import com.sut.se.g13.Repository.ProvinceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Collection;
-
+//import java.util.Optional;
 import java.util.stream.Collectors;
-
-import com.sut.se.g13.Entity.*;
-import com.sut.se.g13.Repository.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class ProvinceController {
-    @Autowired
-    private ProvinceRepository provinceRepository;
 
-    public ProvinceController(
-            ProvinceRepository provinceRepository) {
-        
+    @Autowired
+    private final ProvinceRepository provinceRepository;
+
+    public ProvinceController(ProvinceRepository provinceRepository) {
         this.provinceRepository = provinceRepository;
     }
 
@@ -28,5 +24,4 @@ public class ProvinceController {
     public Collection<Province> provinces() {
         return provinceRepository.findAll().stream().collect(Collectors.toList());
     }
-
 }
